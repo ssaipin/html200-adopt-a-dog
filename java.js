4455c806c1995a690097dd039dcfe96a0f072853
Gao Saipin <gao_gmm@live.com>
2025-05-16 13:55:02 -0700

    created java.js, add dog info when clicking dog's img, add total when clicking adopt btn and reset total when clicking submit btn in checkout page

---
+++ java.js
@@ -0,0 +1,55 @@
+// alert dog's info when clicking on the dog's photo
+function showInfo(name, breed, fee) {
+    alert("Name: " + name +
+        "\nBreed: " + breed +
+        "\nFee: " + fee);
+}
+
+const dogImgs = document.querySelectorAll('.item img').forEach((img) => {
+    img.addEventListener('click', () => {
+        const card = img.closest('.item');
+
+        const name = card.querySelector('.dog-name').textContent;
+        const breed = card.querySelector('.breed').textContent;
+        const fee = card.querySelector('.fee').textContent;
+
+        showInfo(name, breed, fee);
+
+    });
+});
+
+// Load total from sessionStorage 
+let total = sessionStorage.getItem('total') 
+    ? parseFloat(sessionStorage.getItem('total')) 
+    : 0;
+
+const totalDisplays = document.getElementsByClassName('total-cost');
+
+function updateTotalDisplays() {
+  for (let i = 0; i < totalDisplays.length; i++) {
+    totalDisplays[i].textContent = `$${total.toFixed(2)}`;
+  }
+}
+updateTotalDisplays(); // update initially on page load
+
+// Add fee to total
+const adoptButtons = document.querySelectorAll('.item button');
+adoptButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const card = button.closest('.item');
+    const cost = parseFloat(card.querySelector('.fee').textContent.replace('$', ''));
+
+    total += cost;
+    sessionStorage.setItem('total', total.toFixed(2)); // store in session
+
+    updateTotalDisplays(); // update all matching elements
+    alert(`Your current total fee: $${total.toFixed(2)}`); // alert the update total
+  });
+});
+
+// Reset the total after click submit button in checkout page
+function resetTotal() {
+  total = 0;
+  sessionStorage.removeItem('total');
+  updateTotalDisplays();
+}
